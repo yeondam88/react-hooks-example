98c7f9b01b2d4133ace2a3c66129f3eba1a14408
Yeondam Park <giyen9088@gmail.com>
2018-12-13 22:53:33 -0800

    setting up react app with creat-react-app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Typography } from "@material-ui/core";
+import TodoForm from "./TodoForm";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Typography component="h1" variant="h2">
+          Todos
+        </Typography>
+
+        <TodoForm saveTodo={console.warn} />
       </div>
     );
   }
