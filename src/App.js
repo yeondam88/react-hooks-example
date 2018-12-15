3e133e235542c32fa5765ed35f2ecebeda77ab3c
Yeondam Park <giyen9088@gmail.com>
2018-12-14 22:25:48 -0800

    react hooks API test..

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
+import React, { useState } from "react";
 import "./App.css";
-import { Typography } from "@material-ui/core";
-import TodoForm from "./TodoForm";
+import Button from "@material-ui/core/Button";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Typography component="h1" variant="h2">
-          Todos
-        </Typography>
+function App() {
+  const [counter, setCounter] = useState(0);
+  return (
+    <div className="App">
+      <h1>React Hooks API</h1>
 
-        <TodoForm saveTodo={console.warn} />
+      <p className="counter">{counter}</p>
+      <div className="button-group">
+        <Button
+          onClick={() => setCounter(prevCount => prevCount + 1)}
+          color="primary"
+        >
+          +
+        </Button>
+        <Button
+          onClick={() => setCounter(prevCount => prevCount - 1)}
+          color="secondary"
+        >
+          -
+        </Button>
+        <Button onClick={() => setCounter(0)} color="secondary">
+          Reset
+        </Button>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default App;
